378237dcb06bc91aa23f2bc64f367d48fe199572
bartlomiejbloniarz <bartlomiej.bloniarz@student.uj.edu.pl>
2020-10-25 14:15:34 +0100

    issues and reading from file

--- shell/src/mshell.c
+++ shell/src/mshell.c
@@ -3,6 +3,8 @@
 #include <wait.h>
 #include <errno.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 #include "config.h"
 #include "siparse.h"
@@ -15,56 +17,93 @@ main(int argc, char *argv[])
 {
 	pipelineseq * ln;
 	command *com;
-	int readSize, wStatus;
+	int readSize, wStatus, offset = 0, start = 0, stop;
+	char syntaxError = 0;
 	pid_t pid;
+	struct stat stat;
+	fstat(STDIN, &stat);
 
-	char buf[MAX_LINE_LENGTH+1];
+	char buf[2*MAX_LINE_LENGTH+2];
 
-    write(STDOUT, PROMPT_STR, sizeof(PROMPT_STR));
-	while ((readSize = read(STDIN, buf, MAX_LINE_LENGTH+1))){
+	if (S_ISCHR(stat.st_mode))
+        write(STDOUT, PROMPT_STR, sizeof(PROMPT_STR));
+	while ((readSize = read(STDIN, buf + offset, MAX_LINE_LENGTH+1))){
 
-	    buf[readSize] = 0;
-	    ln = NULL;
+	    if (readSize==-1)
+	        return 1;
 
-	    if (readSize<=MAX_LINE_LENGTH)
-	        ln = parseline(buf);
+        readSize += offset;
+	    while((stop = findEndLine(buf, start, readSize))!=-1) {
 
-		if (ln==NULL){
-            write(STDERR, SYNTAX_ERROR_STR, sizeof(SYNTAX_ERROR_STR));
-            write(STDERR, "\n", sizeof("\n"));
-            continue;
-		}
+	        if (syntaxError){
+	            syntaxError = 0;
+	            writeSyntaxError();
+	            start = stop + 1;
+                continue;
+	        }
 
-        com = pickfirstcommand(ln);
+            ln = NULL;
+            buf[stop] = 0;
+            if (stop - start <= MAX_LINE_LENGTH) {
+                ln = parseline(buf + start);
+            }
+            start = stop + 1;
 
-		pid = fork();
-		if (pid==0){
-		    int i=0;
-		    char* tab[argseqLength(com->args)+1];
-		    argseq* temp = com->args;
-            do{
-                tab[i]=temp->arg;
-                i++;
-                temp = temp->next;
-            }while(temp!=com->args);
-            tab[i]=NULL;
-            execvp(com->args->arg, tab);
-
-            switch (errno) {
-                case ENOENT: writeErrorForProgram(com->args->arg, "no such file or directory\n"); break;
-                case EACCES: writeErrorForProgram(com->args->arg, "permission denied\n"); break;
-                default: writeErrorForProgram(com->args->arg, "exec error\n"); break;
+            if (ln == NULL) {
+                writeSyntaxError();
+                continue;
             }
 
-            return EXEC_FAILURE;
-		}
-		else{
-		    waitpid(pid, &wStatus, 0);
-		}
-        write(STDOUT, PROMPT_STR, sizeof(PROMPT_STR));
+            com = pickfirstcommand(ln);
+
+            pid = fork();
+            if (pid == 0) {
+                int i = 0;
+                char *tab[argseqLength(com->args) + 1];
+                argseq *temp = com->args;
+                do {
+                    tab[i] = temp->arg;
+                    i++;
+                    temp = temp->next;
+                } while (temp != com->args);
+                tab[i] = NULL;
+
+                execvp(com->args->arg, tab);
+
+                switch (errno) {
+                    case ENOENT:
+                        writeErrorForProgram(com->args->arg, "no such file or directory\n");
+                        break;
+                    case EACCES:
+                        writeErrorForProgram(com->args->arg, "permission denied\n");
+                        break;
+                    default:
+                        writeErrorForProgram(com->args->arg, "exec error\n");
+                        break;
+                }
+
+                return EXEC_FAILURE;
+            } else {
+                waitpid(pid, &wStatus, 0);
+            }
+        }
+	    offset = readSize;
+        if (S_ISCHR(stat.st_mode))
+            write(STDOUT, PROMPT_STR, sizeof(PROMPT_STR));
+
+        if (offset>MAX_LINE_LENGTH){
+            if (offset - start>MAX_LINE_LENGTH){
+                offset = 0;
+                syntaxError = 1;
+            }
+            else {
+                buf[offset] = 0;
+                mystrcpy(buf, buf + start);
+                offset -= start;
+            }
+            start = 0;
+        }
+
 	}
-
-	write(STDOUT, "\n", sizeof("\n"));
-
 	return 0;
 }
